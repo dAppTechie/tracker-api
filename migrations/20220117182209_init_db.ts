40817d5ae09b5d9b0d6b43ba5c7908112b2d1910
Chad Parker <63312621+imchad-me@users.noreply.github.com>
2022-01-17 12:36:31 -0700

    Completed

--- migrations/20220117182209_init_db.ts
+++ migrations/20220117182209_init_db.ts
@@ -1,22 +1,19 @@
-import { Knex } from "knex";
-
+import { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
-    await knex.schema.createTable('users', table => {
-        table.specificType('id', 'CHAR(16)').primary();
-        table.string('email').notNullable().unique();
-        table.string('firstName').notNullable();
-        table.string('lastName').notNullable();
-        table.string('password').notNullable();
-        table.string('department');
-        table.boolean('isAdmin').defaultTo('false').notNullable();
-        table.timestamp('createdAt').defaultTo(knex.fn.now()).notNullable();
-        table.timestamp('updateAt').notNullable();
-    })
+  await knex.schema.createTable('users', (table) => {
+    table.specificType('id', 'CHAR(16)').primary();
+    table.string('email').notNullable().unique();
+    table.string('firstName').notNullable();
+    table.string('lastName').notNullable();
+    table.string('password').notNullable();
+    table.string('department');
+    table.boolean('isAdmin').defaultTo('false').notNullable();
+    table.timestamp('createdAt').defaultTo(knex.fn.now()).notNullable();
+    table.timestamp('updatedAt').notNullable();
+  });
 }
-
 
 export async function down(knex: Knex): Promise<void> {
-    await knex.schema.dropTable('users');
+  await knex.schema.dropTable('users');
 }
-
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,43 @@
+import { PrismaClient } from '@prisma/client';
 import express from 'express';
 import morgan from 'morgan';
+import { nanoid } from 'nanoid';
+
+const db = new PrismaClient({ log: ['error', 'info', 'query', 'warn'] });
+const genId = () => nanoid(16);
+
+const seedDatabase = async () => {
+  if ((await db.user.count()) === 0) {
+    await db.user.createMany({
+      data: [
+        {
+          id: genId(),
+          email: 'test@testing.com',
+          firstName: 'John',
+          lastName: 'Doe',
+          password: 'password',
+          createdAt: new Date(),
+        },
+        {
+          id: genId(),
+          email: 'test2@testing.com',
+          firstName: 'Jane',
+          lastName: 'Doe',
+          password: 'password',
+          createdAt: new Date(),
+        },
+      ],
+    });
+  }
+};
+seedDatabase();
 
 const app = express();
 app.use(morgan('dev'));
 
-app.get('/', (req, res) => {
-  res.json({ hello: 'World' });
+app.get('/', async (req, res) => {
+  const users = await db.user.findMany();
+  res.json(users);
 });
 
 const PORT = Number(process.env.PORT ?? 8080);
