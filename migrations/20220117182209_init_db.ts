a2b841475eb6b658e400074a86675379816bb306
Chad Parker <63312621+imchad-me@users.noreply.github.com>
2022-01-17 11:58:37 -0700

    Added knex and initial db migration

---
+++ migrations/20220117182209_init_db.ts
@@ -0,0 +1,22 @@
+import { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+    await knex.schema.createTable('users', table => {
+        table.specificType('id', 'CHAR(16)').primary();
+        table.string('email').notNullable().unique();
+        table.string('firstName').notNullable();
+        table.string('lastName').notNullable();
+        table.string('password').notNullable();
+        table.string('department');
+        table.boolean('isAdmin').defaultTo('false').notNullable();
+        table.timestamp('createdAt').defaultTo(knex.fn.now()).notNullable();
+        table.timestamp('updateAt').notNullable();
+    })
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+    await knex.schema.dropTable('users');
+}
+
