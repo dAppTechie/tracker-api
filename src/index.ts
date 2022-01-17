0552e47d29fecdf14a1f2cfe3d5af1f02943b1ca
Chad Parker <63312621+imchad-me@users.noreply.github.com>
2022-01-17 09:17:34 -0700

    Add docker

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const app = express();
 app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
-  res.json({ hello: 'world' });
+  res.json({ hello: 'World' });
 });
 
 const PORT = Number(process.env.PORT ?? 8080);
